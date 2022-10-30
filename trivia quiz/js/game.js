d1e08b9990fedc00602826e2bf31801289d6ca04
leamor96 <116647096+leamor96@users.noreply.github.com>
2022-10-30 18:57:20 +0200

    Add files via upload

--- trivia quiz/js/game.js
+++ trivia quiz/js/game.js
@@ -12,6 +12,7 @@ let availableQuestion=[]
 
 let questions = [
   {
+    //1
     question: "When was the first Mardi Gras?",
     choice1: "Feb. 24, 1857",
     choice2: "Jan. 20, 1957",
@@ -20,6 +21,7 @@ let questions = [
     answer: 1,
   },
   {
+    //2
     question: "What are the Mardi Gras colors?",
     choice1: "red, blue and green",
     choice2: "gold, pink and yellow",
@@ -28,6 +30,7 @@ let questions = [
     answer: 3,
   },
   {
+    //3
     question: "What is Twelfth Night?",
     choice1: "The night of the celebrations ending",
     choice2: "The official start of Mardi Gras",
@@ -36,17 +39,73 @@ let questions = [
     answer: 2,
   },
   {
+    //4
     question: "How does Mardi Gras benefit the New Orleans economy?",
     choice1: "one hundred fifty million",
     choice2: "three million",
     choice3: "five hundred thousand",
     choice4: "over $1 billion",
     answer: 4,
-  }
-]
+  },
+  {
+    //5
+    question: "What is the signature Mardi Gras dessert?",
+    choice1: "King cake",
+    choice2: "Pancakes",
+    choice3: "S'mores dip",
+    choice4: "Ice cream",
+    answer: 1,
+  },
+  {
+    //6
+    question: "What does purple signify during Mardi Gras?",
+    choice1: "Power",
+    choice2: "Faith",
+    choice3: "Blossom",
+    choice4: "Justice",
+    answer: 4,
+  },
+  {
+    //7
+    question: "What is traditionally hidden inside a king cake?",
+    choice1: "A plastic baby",
+    choice2: "Santa Claus figurine",
+    choice3: "A Crown",
+    choice4: "A plastic candy",
+    answer: 1,
+  },
+  {
+    //8
+    question: "How many king cakes are sold during an average carnival season?",
+    choice1: "1.2 million",
+    choice2: "500,000",
+    choice3: "100,000",
+    choice4: "20,000",
+    answer: 2,
+  },
+  {
+    //9
+    question: "What does gold signify during Mardi Gras?",
+    choice1: "Justice",
+    choice2: "Faith",
+    choice3: "Power",
+    choice4: "Royalty",
+    answer: 3,
+  },
+  {
+    //10
+    question:
+      "How many times have Mardi Gras parades been canceled in New Orleans?",
+    choice1: "13",
+    choice2: "none",
+    choice3: "2",
+    choice4: "10",
+    answer: 1,
+  },
+];
 
-const SCORE_POINTS= 100
-const MAX_QUESTIONS=4
+const SCORE_POINTS = 10;
+const MAX_QUESTIONS = 10;
 
 startGame = () => {
     questionCounter=0
